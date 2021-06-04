ed407ce73d86df8f835a3b6f836de3e20bad155c
Altricia Latimer <latimers.learners@gmail.com>
2021-06-03 22:30:12 -0400

    Update app.js

--- code folder/assets/js/app.js
+++ code folder/assets/js/app.js
@@ -1,1 +1,5 @@
-// @TODO: YOUR CODE HERE!
+//Set default x/y axis varaiables.
+var chosenXAxis ="poverty";
+var chosenYAxis ="healthcare";
+// Function used for updating x-scale var upon click on axis label.
+function xScale(data, chosenXAxis, chartWidth) {

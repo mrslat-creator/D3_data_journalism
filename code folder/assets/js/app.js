e37355f0f9581b9e83ebf230f8987927c8619a6b
Altricia Latimer <latimers.learners@gmail.com>
2021-06-03 22:42:23 -0400

    Update app.js

--- code folder/assets/js/app.js
+++ code folder/assets/js/app.js
@@ -3,3 +3,12 @@ var chosenXAxis ="poverty";
 var chosenYAxis ="healthcare";
 // Function used for updating x-scale var upon click on axis label.
 function xScale(data, chosenXAxis, chartWidth) {
+  // Creat scales.
+  var xLinearScale = d3.scaleLinear()
+      .domain([d3.min(data, d => d[chosenXAxis]) *.8,
+               d3.max(data, d => d[chosenXAxis]) *1.1])
+      .range([0, chartWidth]);
+  return xLinearScale;
+}
+// Function used for updating xAxis var upon click on axis label.
+function renderXAxis

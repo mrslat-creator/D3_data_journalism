4e1df3566cd1bb0bdb2a76c56bc9921d30063a7d
Altricia Latimer <altricia.latimer@gmail.com>
2021-06-04 21:07:43 -0400

    Update app.js

--- code folder/assets/js/app.js
+++ code folder/assets/js/app.js
@@ -11,4 +11,19 @@ function xScale(data, chosenXAxis, chartWidth) {
   return xLinearScale;
 }
 // Function used for updating xAxis var upon click on axis label.
-function renderXAxis
+function renderXAxis(newXScale, xAxis) {
+  var bottomAxis = d3.axisBottom(newXScale);
+  xAxis.transition()
+       .duration(1000)
+       .call(bottomAxis);
+    return xAxis;
+}
+// Function used for updating y-scale var upon click on axis label.
+function yScale(data, chosenYAxis, chartHeight) {
+  //Create Scales
+  var yLinearScale = d3.scaleLinear()
+      .domain([d3.min(data, d => d[chosenYAxis]) *8,
+      d3.max(data, d => d[chosenYAxis]) *1.2])
+    .range([chartHeight, 0]);
+    return yLinearScale;
+}

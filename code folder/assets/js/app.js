9eea0a3c8f63938533b6751d8a8d068005987097
Altricia Latimer <altricia.latimer@gmail.com>
2021-06-22 20:46:07 -0400

    Update app.js

--- code folder/assets/js/app.js
+++ code folder/assets/js/app.js
@@ -1,4 +1,5 @@
-//function to resize chart
+
+//Function to resize chart
 function responsiveChart() {
 
    //Clear svg area
@@ -42,4 +43,89 @@ function responsiveChart() {
     //Read in SVG file and Extract Data
     d3.csv("./assets/data/data.csv").then(function(censusData) {
       console.log(censusData);
-    })
+
+      //Convert data from csv table to numbers for use in scatter plot
+      censusData.forEach(function(data){
+         data.poverty = +data.poverty;
+         data.povertyMoe = +data.povertyMoe;
+         data.age = +data.age;
+         data.ageMoe = +data.ageMoe;
+         data.income = +data.income;
+         data.incomeMoe = +data.incomeMoe;
+         data.healthcare = +data.healthcare;
+         data.healthcareLow = +data.healthcareLow;
+         data.healthcareHigh = +data.healthcareHigh;
+         data.obesity = +data.obesity;
+         data.obesityLow = +data.obesityLow;
+         data.obesityHigh = +data.obesityHigh;
+         data.smokes = +data.smokes;
+         data.smokesLow = +data.smokesLow;
+         data.smokesHigh = +data.smokesHigh;
+      });
+
+      //Create scale functions for Healthcare on xAxis and Poverty on yAxis
+      //Healthcare scale on xAxis
+      var xLinearScale = d3.scaleLinear()
+          .domain([d3.min(censusData, d => d.healthcare) - 1, d3.max(censusData, d=> d.healthcare) + 1])
+          .range([0, chartWidth]);
+      
+      //Poverty scale on yAxis
+      var yLinearScale = d3.scaleLinear()
+          .domain([d3.min(censusData, d => d.poverty) - 1, dr.max(censusData, d => d.poverty) + 1])
+          .range([chartHeight, 0]);
+              
+      //Create axis functions
+      var bottomAxis = d3.axisBottom(xLinearScale);
+      var leftAxis = d3.axisLeft(yLinearScale);
+
+      //Append axis to chart
+      //Bottom is translated down to bottom of the chart and y axis by chart height
+      chartGroup.append("g")
+           .attr("transform", `translate(0, ${chartHeight})`)
+           .call(bottomAxis);
+      //leftAxis is place on left
+      chartGroup.append("g")
+         .call(leftAxis);
+      
+      //Establish a color scale 
+      var colorScale =d3.scaleLinear()
+      .domain([d3.min(censusData, d => d.obesity), d3.mean(censusData, d => d.obesity), d3max(censusData, d=> d.obesisty)])
+      .range(["green", "yellow", "red"]);
+      
+      //Tool tip implementation
+      var tool_tip = d3.tip()
+          .attr("class", "d3-tip")
+          .offset([0,50])
+          .html(function(d) { return `${d.state}<hr>$${d.income}<hr>Obesity Index:  ${d.obesity}, Smoking Index: $ {d.smokes}`}); 
+    
+      svg.call(tool_tip);
+      
+      
+      //Create circles for plotting
+      var circleGroup = chartGroup.selectAll("circle")
+          .data(censusData)
+          .enter()
+
+          circleGroup
+          .append("circle")
+          .attr("class", "circle")
+          .attr("cx", d => xLinearScale(d.healthcare))
+          .attr("cy", d => yLinearScale(d.poverty))
+          .attr("r", d => ((d.income*d.income)/110000000))
+          .attr("fill", function(d) { return colorScale(d.obesity); })
+          .attr("opacity", "0.7")
+          .style("stoke", "black")
+
+        //Create labels using abbrevations
+           circleGroup
+           .append("text")
+           .text(function(d){
+             return d.abbr;
+           })
+           .attr("x", d => xLinearScale(d.healthcare) - 11)
+           .attr("y", d => yLinearScale(d.poverty) + 6)
+           
+           .on("click", tool_tip.show)
+           .on("mouseout", tool_tip.hide)
+        //
+})

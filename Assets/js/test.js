163012acaa7cf567f2b196d5c5d7e61866240b15
Altricia Latimer <altricia.latimer@gmail.com>
2021-06-23 21:15:36 -0400

    Update test.js

--- Assets/js/test.js
+++ Assets/js/test.js
@@ -23,6 +23,7 @@ var chartGroup = svg.append("g")
 //Import Data
 d3.csv("Assets/data/data.csv").then(function(demoData) {
   console.log(demoData);
+  //Parse Data
   demoData.forEach(function(data){
     data.poverty = +data.poverty;
     data.healthcare = +data.healthcare;
@@ -34,7 +35,65 @@ d3.csv("Assets/data/data.csv").then(function(demoData) {
     .domain([20, d3.max(demoData, d => d.poverty)])
     .range([0, width]);
   
-  var yLinearScale = d3.scaleLinear()
+  var yLinearScale = d3.scaleLinear() 
+    .domain([0, d3.max(demoData, d => d.healthcare)])
+    .range([height, 0]);
+  
+  //Create axis functions
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+  
+  //Append Axes to chart
+  chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+  chartGroup.append("g")
+     .call(leftAxis);
+  
+  //Create Circles
+  var circlesGroup = chartGroup.selectAll("circle")
+  .data(demoData)
+  .enter()
+  .append("circle")
+  .attr("cx", d => xLinearScale(d.poverty))
+  .attr("cy", d => yLinearScale(d.healthcare))
+  .attr("r", "15")
+  .attr("fill", "pink")
+  .attr("opacity", ".5");
+
+  //Init Tool Tip
+  var toolTip = d3.tip()
+  .attr("class", "toolTip")
+  .offset([80, -60])
+  .html(function(d) {
+    return (`${d.state}<br>Poverty: ${d.poverty}<br>Healthcare: ${d.healthcare}`);
+  });
+
+  //Create toolTip in the chart
+  chartGroup.call(toolTip);
+
+  //Create event listeners to display and hide tooltips
+  circlesGroup.on("click", function(data) {
+    toolTip.show(data, this);
+  })
+    //On MouseOut event
+    .on("mouseout", function(data, index) {
+      toolTip.hide(data);
+    });
+  //Create axes labels
+  chartGroup.append("text")
+    .attr("transform", "rotate(-90")
+    .attr("y", 0 - margin.left + 40)
+    .attr("x", 0 - (height /2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("State by State Breakdown of Poverty");
+
+  chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    .attr("class", "axisText")
+    .text("State by State Breakdown of Healthcare");
 }).catch(function(error) {
   console.log(error);
 });
